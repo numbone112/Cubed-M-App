909711e65b1e41962c521f780ea3f416a1bac459
sodiffi <44388952+sodiffi@users.noreply.github.com>
2023-04-22 21:29:05 +0800

    修正 - 移除錯誤網址

--- lib/request/api.dart
+++ lib/request/api.dart
@@ -2,6 +2,5 @@ import 'package:http/http.dart' as http;
 
 class API{
   final client = http.Client();
-  // final String domain = "https://e-fu-back.onrender.com";
-  final String domain = "https://18a9-60-250-79-114.ngrok-free.app";
+  final String domain = "https://e-fu-back.onrender.com";
 }
